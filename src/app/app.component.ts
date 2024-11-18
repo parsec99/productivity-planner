9fcb28156e24ca06adfd7a23c24076d8a9d1d221
damienheckler <5891353+damienheckler@users.noreply.github.com>
2024-11-18 09:02:18 +0100

    staging environnement + deployment
    Sprint 0 - Staging environnement, Deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readonly isStaging = !(environment as any).production;
+}
