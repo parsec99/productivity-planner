e0f7559fa74e6e77d9ddb3dd3530e6824ef3b564
Damien Heckler <5891353+parsec99@users.noreply.github.com>
2024-11-26 21:57:50 +0100

    Fix ci/cd (#4)
    * chore(project) : add changelog file
    
    * fix ci/cd issue
    
    * fix audit vulnerabilities
    
    * fix test:ci issue
    
    * fix default config = development
    
    * remove isStaging variable
    
    * defaullt config to production
    
    * remove environment prod file replacement
    
    * remove isStaging in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,4 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly isStaging = !(environment as any).production;
-}
+export class AppComponent {}
