83ec84b16a65ccbe19b7ab4426666aa533eb51eb
damienheckler <5891353+parsec99@users.noreply.github.com>
2024-12-01 13:57:21 +0100

    fix(test): missing http provider testing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideHttpClientTesting(),
   ],
 };
